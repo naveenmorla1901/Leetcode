4b7c660784693446b0e96f751d5f16ab12faf6be
Naveen Morla <61311458+naveenmorla1901@users.noreply.github.com>
2024-01-25 18:23:12 -0500

    Update 0036-valid-sudoku.py

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,8 +1,8 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols=collections.defaultdict(set)
+        cols=collections.defaultdict(set) # Create HashSet for every row and col and check if the element exists or not in this row and colms.
         rows=collections.defaultdict(set)
-        squares=collections.defaultdict(set)
+        squares=collections.defaultdict(set) # for 0 to 8 when we divide an element with 3 we get a value of 0 to 2, according to that we can store elements in a hash map example for squares[0][1] we can save a list of elements which can be 1 to 9 without repeating themselves.  
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
